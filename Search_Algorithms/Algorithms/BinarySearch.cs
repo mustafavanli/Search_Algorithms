f6e61c7704fcd7b9359e1f2c5f75ac8052999428
Mustafa Vanlı <vanlimustafa0@gmail.com>
2023-01-26 02:00:06 +0300

    Add project files.

---
+++ Search_Algorithms/Algorithms/BinarySearch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Search_Algorithms.Algorithms
+{
+    internal class BinarySearch
+    { 
+        // this is recursive method (yinelemeli method)
+        public static int Search(int[] array, int searchValue,int low,int high)
+        {
+            int median = (low + high) / 2;
+
+            if (array[median] == searchValue)
+            {
+                return median;
+            }
+            else if (array[median] < searchValue)
+            {
+                return Search(array,searchValue, median + 1,high);
+            }
+            else if (array[median] > searchValue)
+            {
+                return Search(array, searchValue, median, high -1 );
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Search_Algorithms/Program.cs
@@ -0,0 +1,10 @@
+﻿using Search_Algorithms.Algorithms;
+
+int[] numbers = new int[] {1,3,5,8,10,15,24,25};
+int value = 25;
+
+#region Binary Search
+int result = BinarySearch.Search(numbers, value, 0,numbers.Count()-1);
+Console.WriteLine($"Aranan değer şu indeksde: {result}");
+#endregion
+Console.Read();
